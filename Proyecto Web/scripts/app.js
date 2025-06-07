52c26ef0c87cca57c7f940bb51649edb9191382d
lucio aranda <lucio.aranda@istea.com.ar>
2025-06-06 23:49:31 -0300

    update_counter-js

--- Proyecto Web/scripts/app.js
+++ Proyecto Web/scripts/app.js
@@ -6,13 +6,36 @@ document.addEventListener('DOMContentLoaded', () => {
     const filter = searchInput.value.toLowerCase();
 
     productCards.forEach(card => {
-      
       const title = card.querySelector('.title-product').textContent.toLowerCase();
-
       if (title.includes(filter)) {
-        card.style.display = ''; 
+        card.style.display = '';
       } else {
-        card.style.display = 'none'; 
+        card.style.display = 'none';
+      }
+    });
+  });
+
+  const counters = document.querySelectorAll('.counter');
+
+  counters.forEach(counter => {
+    const btnMinus = counter.querySelector('.btn-minum'); 
+    const btnPlus = counter.querySelector('.btn-plus');
+    const quantity = counter.querySelector('.quantity');
+
+    if (!btnMinus || !btnPlus || !quantity) {
+      console.warn('Faltan elementos en counter:', counter);
+      return; 
+    }
+
+    btnPlus.addEventListener('click', () => {
+      let currentValue = parseInt(quantity.textContent) || 0;
+      quantity.textContent = currentValue + 1;
+    });
+
+    btnMinus.addEventListener('click', () => {
+      let currentValue = parseInt(quantity.textContent) || 0;
+      if (currentValue > 1) {
+        quantity.textContent = currentValue - 1;
       }
     });
   });
